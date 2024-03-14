ecef831b7aed509fa381cec5da2854c0c7139602
anestistzoyr <anestistoyr@arx.net>
2024-03-14 10:53:08 +0200

    Add - Nodemon for easier development

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const http = require("http");
 
 const express = require("./rest.js");
 
+const PORT = process.env.PORT || 3000;
+
 const server = http.createServer(express);
 
-server.listen(3000);
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
