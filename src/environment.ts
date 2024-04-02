77e4c555325544cc2d9fc9c0a88ba8d45900fc7e
Anastasios Tzouras <anestistzoyr@gmail.com>
2024-04-02 10:57:58 +0300

    Add - Environment File that contains the app's specifications

---
+++ src/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    apiUrl: 'http://localhost:3000',
+    production: false,
+    appVersion: "v0.0.1",
+    features: {
+        appointmentsEnabled: false,
+        classRegistrationEnabled: true,
+    }
+};
