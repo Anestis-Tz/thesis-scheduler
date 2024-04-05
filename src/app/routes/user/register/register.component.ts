316f122ebc773e57d9caf4e8e6a6543ff8f75dd9
Anastasios Tzouras <anestistzoyr@gmail.com>
2024-04-06 01:51:13 +0300

    Add - Confirm password and terms field in register form, validator file that checks for password equality

--- src/app/routes/user/register/register.component.ts
+++ src/app/routes/user/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControlOptions } from '@angular/forms';
 import { AuthService } from '../../../services/auth.service';
+import { ConfirmedValidator } from '../../../validators/ConfirmedValidator';
 
 @Component({
   selector: 'app-register',
@@ -9,21 +10,27 @@ import { AuthService } from '../../../services/auth.service';
 })
 export class RegisterComponent {
   registerGroup: FormGroup;
-  hide = true;
+  hidePassword = true;
+  hideConfirmPassword = true;
 
   constructor(private formBuilder: FormBuilder, private authService: AuthService) {
     this.registerGroup = this.formBuilder.group({
       username: ['', Validators.required],
       email: ['', [Validators.required, Validators.email, Validators.pattern('^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}$')]],
       password: ['', Validators.required],
-      // confirmPassword: ['', Validators.required],
-      // privacy: [false, Validators.requiredTrue]
-    });
+      confirmPassword: ['', Validators.required],
+      terms: [false, Validators.requiredTrue]
+    }, {
+      validators: ConfirmedValidator('password', 'confirmPassword')
+    } as AbstractControlOptions);
   }
 
   register() {
-    // console.log("sas")
-    // if (!this.registerGroup.valid) return;
-    this.authService.createUser(this.registerGroup.value.username, this.registerGroup.value.email, this.registerGroup.value.password);
+    if (this.registerGroup.valid) {
+      this.authService.createUser(
+        this.registerGroup.value.username, 
+        this.registerGroup.value.email, 
+        this.registerGroup.value.password);
+    }
   }
 }
