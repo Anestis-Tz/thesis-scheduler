e183fae9b4a2064ed860a0663db603acf6508286
Anastasios Tzouras <anestistzoyr@gmail.com>
2024-04-16 19:52:31 +0300

    Modified token authentication

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -1,20 +1,27 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-    const token = req.headers.authorization.split(" ")[1];
-    let decodedToken;
-    try {
-        decodedToken = jwt.verify(token, "secret_this_should_be_longer_like_very_longer");
-    } catch(err) {
-        return res.status(401).json({
-            message: "Auth failed"
-        });
+    if (!req.headers.authorization) {
+        return res.status(401).json({ message: "Auth token missing" });
     }
-    if (!decodedToken) {
-        return res.status(401).json({
-            message: "Auth failed"
-        });
+    const parts = req.headers.authorization.split(" ");
+    if (parts.length === 2 && parts[0] === "Bearer") {
+        const token = parts[1];
+        let decodedToken;
+        try {
+            decodedToken = jwt.verify(token, "secret_this_should_be_longer_like_very_longer");
+        } catch (err) {
+            return res.status(401).json({
+                message: "Auth failed",
+                error: err.message // Adjust according to environment
+            });
+        }
+        if (!decodedToken) {
+            return res.status(401).json({ message: "Auth failed" });
+        }
+        req.userId = decodedToken.userId;
+        next();
+    } else {
+        return res.status(401).json({ message: "Auth token malformed" });
     }
-    req.userId = decodedToken.userId;
-    next();
-}
+};
