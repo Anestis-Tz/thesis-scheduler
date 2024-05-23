c60e99f677badbdd3f70c55d16e83b0ac3ce630a
Anastasios Tzouras <anestistzoyr@gmail.com>
2024-05-23 14:11:03 +0300

    Add - Display footer only if we are in home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { environment } from '../environment';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
 })
+
 export class AppComponent {
   features = environment.features;
-  constructor(private translateService: TranslateService) {
+  isHomeRoute: boolean = false;
+
+  constructor(private translateService: TranslateService, private router: Router) {
     this.translateService.setDefaultLang('en');
     this.translateService.use(localStorage.getItem('lang') || 'en');
+
+    this.router.events.subscribe((routerEvent) => {
+      if (routerEvent instanceof NavigationEnd) {
+        this.isHomeRoute = this.router.url == '/home';
+      }
+    });
+
   }
+
   title = 'thesis-scheduler';
 }
